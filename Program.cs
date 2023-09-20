371dfd51821c1d6fdf3b85e72f656affb4db43b0
unknown <Oreahesso>
2023-09-20 09:53:00 +0300

    do two tasks

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+// Console.WriteLine ("Input number 1: ");
+// int num1 = Convert.ToInt32 (Console.ReadLine());
+
+// Console.WriteLine("Input number 2: ");
+// int num2 = Convert.ToInt32 (Console.ReadLine());
+
+// if (num1 > num2)
+// {
+//     Console.WriteLine($"Number 1 ({num1}) more then Number 2 ({num2})");
+// }
+// else{
+//     Console.WriteLine($"Number 2 ({num2}) more then Number 1 ({num1})");
+// }
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine ("Input number 1: ");
+int num1 = Convert.ToInt32 (Console.ReadLine());
+
+Console.WriteLine("Input number 2: ");
+int num2 = Convert.ToInt32 (Console.ReadLine());
+
+Console.WriteLine("Input number 3: ");
+int num3 = Convert.ToInt32 (Console.ReadLine());
+
+int biggest = num1;
+
+if (num2>num1)
+{
+    biggest = num2;
+}
+if (num3>biggest)
+{
+    biggest = num3;
+}
+Console.WriteLine($"Biggest number is {biggest}");
